4eb68751df7488e31bb28dbeb3093294987e4b53
Ross Edfort <rossedfort@gmail.com>
2015-09-28 10:55:01 -0600

    full functionality with http_yeah_you_know_me

--- lib/night_write.rb
+++ lib/night_write.rb
@@ -2,19 +2,23 @@ require_relative 'translator'
 require 'pry'
 
 class NightWrite
+  attr_reader :message
 
   def get_braille
     Translator.new.translate
   end
 
   def convert_to_braille(braille_line)
-    @text = braille_line.split("")
     @letters = get_braille
-    @text.map {|e| @letters.fetch(e)[line]}.flatten.join
+    @text.map {|e| @letters.fetch(e)[braille_line]}.flatten.join
+  end
+
+  def write(message)
+    @text = message.split("") # => ["h", "e", "l", "l", "o"]
+
     line_zero = convert_to_braille(0)
     line_one = convert_to_braille(1)
     line_two = convert_to_braille(2)
     block = [line_zero, line_one, line_two].join("\n")
   end
-
 end
